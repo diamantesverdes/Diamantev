3c4ffcf21556d5750715a12f280dfa1872c7f77b
diamantesverdes <diamantesverdes.ec@gmail.com>
2026-07-23 23:11:54 -0500

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -479,11 +479,13 @@ export default function Admin() {
        <hr className="admin-divider" />. 
       <div className="admin-subtabs">
             <button className={invSubTab === 'stock' ? 'active' : ''}
-     <div className="admin-subtabs">
+    <hr className="admin-divider" />.
+              <div className="admin-subtabs">
             <button className={invSubTab === 'stock' ? 'active' : ''}
               <><div className="admin-subtabs">
             <button className={invSubTab === 'stock' ? 'active' : ''}
-          <div className="admin-subtabs">
+         <hr className="admin-divider" />.
+              <div className="admin-subtabs">
             <button className={catSubTab === 'categories' ? 'active' : ''} onClick={() => setCatSubTab('categories')}>Categorías</button>
             <button className={catSubTab === 'gallery' ? 'active' : ''} onClick={() => setCatSubTab('gallery')}>Galería</button>
           </div>
