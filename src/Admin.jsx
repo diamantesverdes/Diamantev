d7ed4825805b2226605504efd6c2f0f9c6064aee
diamantesverdes <diamantesverdes.ec@gmail.com>
2026-07-27 16:35:37 -0500

    Change label from 'Foto' to 'Imagen' in Admin

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -771,8 +771,8 @@ export default function Admin() {
                               <label>Stock: <input type="number" defaultValue={p.stock} onBlur={e => updateStock(p.id, Number(e.target.value))} /></label>
                             </div>
                             <div className="admin-item-actions">
-                              <label className="icon-btn" title="Subir foto">
-                                📷 Foto
+                              <label className="file-label" title="Subir imagen">
+                                📷 Imagen
                                 <input
                                   type="file"
                                   accept="image/*"
